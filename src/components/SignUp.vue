524b33e694c7e6b323036caea9f4f3461eb228f6
hartsean <hartsean@gmail.com>
2020-05-11 14:58:31 -0500

    (feat) create signup component button and toggle modal onClick

---
+++ src/components/SignUp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="modal" v-show="value">
+    <div class="container">
+      <div class="modal__title">Sign Up:</div>
+      <form>
+        Name:
+        Email:
+      </form>
+      <button class="btn">Submit</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SignUp',
+  props: {
+    value: {
+      required: true,
+    },
+    methods: {
+      close() {
+        this.$emit('input', !this.value);
+      },
+    },
+  },
+};
+
+</script>
+<style scoped>
+  .modal{
+    border: 1px solid forestgreen;
+  }
+</style>
