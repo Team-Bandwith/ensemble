df2b6174591e19d7d176f8f3163dfade9fbe3f4c
hartsean <hartsean@gmail.com>
2020-05-11 15:02:50 -0500

    (chore) import SignUp component to Home Page view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Ensemble" />
+     <button class="btn" @click="openModal">Sign Up</button>
+     <SignUp v-model="modalOpen"></SignUp>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import SignUp from '@/components/SignUp.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    SignUp,
+  },
+  data() {
+    return {
+      modalOpen: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.modalOpen = !this.modalOpen;
+    },
   },
 };
 </script>
