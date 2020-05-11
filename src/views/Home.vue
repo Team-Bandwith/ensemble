adaf50599942a0803827ee1ad55ed2798cc413c1
hartsean <hartsean@gmail.com>
2020-05-11 18:41:12 -0500

    (feat) import Login component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Ensemble" />
     <SignUp />
+    <Login />
   </div>
     </b-col>
   </b-row>
@@ -14,22 +15,14 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 import SignUp from '@/components/SignUp.vue';
+import Login from '@/components/Login.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
     SignUp,
-  },
-  data() {
-    return {
-      modalOpen: false,
-    };
-  },
-  methods: {
-    openModal() {
-      this.modalOpen = !this.modalOpen;
-    },
+    Login,
   },
 };
 </script>
