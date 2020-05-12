0d44fcb32938ba7fadc5e8bae1f3e357571ce672
hartsean <hartsean@gmail.com>
2020-05-12 12:05:28 -0500

    (refactor) removed import statements for buttons now in sidebar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Ensemble" />
-    <SignUp />
-    <Login />
+
   </div>
     </b-col>
   </b-row>
@@ -14,15 +13,11 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-import SignUp from '@/components/SignUp.vue';
-import Login from '@/components/Login.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
-    SignUp,
-    Login,
   },
 };
 </script>
