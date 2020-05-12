f6ddc7a49397b71a7c69b4aaa51ee0201f9110e4
matthritz <atmatthritz@gmail.com>
2020-05-12 14:06:24 -0500

    (feat)set up basic Jam room frame

--- src/views/Jam.vue
+++ src/views/Jam.vue
@@ -1,10 +1,35 @@
 <template>
-  <b-row>
-    <b-col>
-
-  <div class="jam">
-    <h1>This is the Jam page</h1>
-  </div>
-    </b-col>
-  </b-row>
+  <b-container class="jam">
+    <b-row class="jam-top">
+      <b-col>
+        <div class="">
+          <h1>instrument component</h1>
+        </div>
+      </b-col>
+      <b-col>
+        <div>
+          <h1>chat component</h1>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <h1>band members component</h1>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
