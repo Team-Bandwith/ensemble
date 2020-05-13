3f091b7b08bf1f80371c9f0695ae524fa24a6b0d
matthritz <atmatthritz@gmail.com>
2020-05-12 19:39:18 -0500

    (feat)chat  chat frame created

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,11 +1,36 @@
 <template>
-  
+  <b-container fluid>
+    <div class="chat">
+      <b-row>
+       <div class="messages">
+         <h4>Chat</h4>
+       </div>
+      </b-row>
+      <b-row>
+        <b-form>
+          <div>
+            <b-form inline>
+              <label class="sr-only" for="inline-form-input-name">Name</label>
+                <b-input
+                id="inline-form-input-name"
+                placeholder="Chat"
+              ></b-input>
+                <b-button variant="primary">Send</b-button>
+            </b-form>
+          </div>
+        </b-form>
+      </b-row>
+      </div>
+    </b-container>
 </template>
 
 <script>
 export default {
+  name: 'Chat',
+  component: {
 
-}
+  },
+};
 </script>
 
 <style>
