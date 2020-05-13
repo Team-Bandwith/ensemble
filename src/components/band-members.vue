46c95ff710217c88b7a71feab97e5cb0f462df89
matthritz <atmatthritz@gmail.com>
2020-05-12 22:14:02 -0500

    (feat)band member template created

---
+++ src/components/band-members.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-container>
+    <b-row align-h="end">
+      <b-button variant="primary">Invite</b-button>
+    </b-row>
+    <b-row>
+
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'BandMembers',
+  components: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
