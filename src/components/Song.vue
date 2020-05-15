4803ac616e9fab230f8154897acfc86d970bc59c
hartsean <hartsean@gmail.com>
2020-05-15 14:05:50 -0500

    (refactor) created song component to be rendered in SongsList component

---
+++ src/components/Song.vue
@@ -0,0 +1,101 @@
+<template>
+  <b-container fluid>
+    <div class="song-item">
+        <b-row cols="12">
+          <b-col>
+            <div class="song-id">id:{{ song.id }}</div>
+            <div class="song-title">title:{{ song.name }}</div>
+            <div class="song-url">
+              <div>
+                <div v-if="isPlaying">
+                  <b-button @click="playSong()">Pause</b-button>
+                </div>
+                <div v-else>
+                  <b-button @click="playSong(song.url)">Play</b-button>
+                </div>
+              </div>
+            </div>
+            <div class="song-likes">
+              <b-button @click="likeSong(song.count_likes, song.id)">Like</b-button>
+              <div v-if="this.likes">
+                {{this.likes}}
+               </div>
+              <div v-else>
+                {{ song.count_likes }}
+              </div>
+            </div>
+          <div class="song-created-at"> posted: {{ handleMoment(song.created_at).fromNow() }} </div>
+            <div class="song-comments">
+              <Comment></Comment>
+            </div>
+          </b-col>
+        </b-row>
+      </div>
+  </b-container>
+</template>
+
+<script>
+
+import { request } from 'graphql-request';
+import Comment from './Comment.vue';
+
+const moment = require('moment');
+
+export default {
+  name: 'Song',
+  props: {
+    msg: String,
+    loggedIn: Boolean,
+    song: Object,
+  },
+  components: {
+    Comment,
+  },
+  data() {
+    return {
+      handleMoment: moment,
+      likes: this.song.likes,
+      isPlaying: false,
+      player: null,
+    };
+  },
+  methods: {
+    playSong(src) {
+      if (this.isPlaying) {
+        this.player.pause();
+        this.isPlaying = false;
+      } else {
+        this.player = new Audio(src);
+        this.player.play();
+        this.isPlaying = true;
+      }
+    },
+    handleUpdateLikes() {
+      console.log('render new likes');
+    },
+    likeSong(likes, songId) {
+      // console.log('like', likes);
+      const query = `mutation {
+      likeSong(id: ${songId}) {
+        count_likes
+      }
+    }`;
+      request(`${process.env.NODE_ENV === 'development' ? 'http://localhost:8081' : ''}/api`, query)
+        .then((res) => {
+          this.likes = res.likeSong.count_likes;
+          // console.log('like this song', res);
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style>
+.song-item{
+  padding: 1em;
+}
+.song-title{
+  font-weight: bold;
+}
+</style>
