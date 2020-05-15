0de93450180ed3d04a2c3e8eb26ad7bdefe9d564
Heather Haylett <heatherhaylett@gmail.com>
2020-05-15 15:24:55 -0500

    (feat) imported direct message, friend request, and jam invite components

--- src/views/Inbox.vue
+++ src/views/Inbox.vue
@@ -5,16 +5,34 @@
   <div class="inbox">
     <h1>This is the inbox page</h1>
   </div>
+  <div class="jam-invite">
+    <JamInvite/>
+  </div>
+    <div class="friend-request">
+    <FriendRequest/>
+  </div>
+  <div class="direct-message">
+    <DirectMessage/>
+  </div>
     </b-col>
   </b-row>
 </template>
 
 <script>
+import JamInvite from '../components/JamInvite.vue';
+import FriendRequest from '../components/FriendRequest.vue';
+import DirectMessage from '../components/DirectMessage.vue';
 
 export default {
-  name: 'Inbox',
+  name: 'inbox',
   components: {
-
+    JamInvite,
+    FriendRequest,
+    DirectMessage,
   },
 };
 </script>
+
+<style>
+
+</style>
