aab2f90974c2d42e096a46a4e33c5618b20d4c40
Heather Haylett <heatherhaylett@gmail.com>
2020-05-15 15:26:23 -0500

    (feat) set up jam invite request component, renders h3 tag to page

---
+++ src/components/JamInvite.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Invite to Jam</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'jam-invite',
+  components: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
