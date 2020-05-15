3b3368bef5034ba3d2d1f2c0131c045a472701fe
hartsean <hartsean@gmail.com>
2020-05-15 14:05:07 -0500

    (feat) created and linked comment components to song

---
+++ src/components/Comment.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container fluid>
+    Comments!
+  </b-container>
+</template>
+
+<script>
+
+// import { request } from 'graphql-request';
+
+const moment = require('moment');
+
+export default {
+  name: 'Comment',
+  props: {
+    loggedIn: Boolean,
+    comment: Object,
+  },
+  data() {
+    return {
+      handleMoment: moment,
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+</style>
