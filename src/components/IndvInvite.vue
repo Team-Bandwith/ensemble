c5f627eef9e7f62d19c7d2dffb23ba872776172b
Heather Haylett <heatherhaylett@gmail.com>
2020-05-16 16:12:05 -0500

    (refactor) add space between invite

--- src/components/IndvInvite.vue
+++ src/components/IndvInvite.vue
@@ -1,16 +1,16 @@
 <template>
-  <b-container fluid>
-    <div class="indv-invite">
-        <b-row cols="12">
-        <b-list-group style="width: 1000px;">
-        <b-list-group-item class="d-flex align-items-center">
-        <b-avatar variant="info" :src="invite.url_avatar" class="mr-3"></b-avatar>
-        <span class="mr-auto">{{ invite.username }}
-            invited you to join their <a :href="'jam' + invite.link">jam room</a></span>
+  <b-container >
+    <b-list-group flush>
+      <div class="indv-invite">
+        <b-list-group-item>
+          <b-avatar variant="info" :src="invite.url_avatar" class="mr-3"></b-avatar>
+            <span class="mr-auto">
+              {{ invite.username }} invited you to join their
+              <a :href="'jam' + invite.link">jam room</a>
+            </span>
         </b-list-group-item>
-        </b-list-group>
-        </b-row>
       </div>
+    </b-list-group>
   </b-container>
 </template>
 
