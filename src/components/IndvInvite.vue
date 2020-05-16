4f294ca1660f9a671944c68c4efc2f409d716e0e
Heather Haylett <heatherhaylett@gmail.com>
2020-05-16 14:09:31 -0500

    (feat) add indv invite component

---
+++ src/components/IndvInvite.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container fluid>
+    <div class="indv-invite">
+        <b-row cols="12">
+          <b-col>
+            <div class="invite-id">id:{{ invite.id }}</div>
+            <div class="user-from">title:{{ invite.id_user_from }}</div>
+          </b-col>
+        </b-row>
+      </div>
+  </b-container>
+</template>
+
+<script>
+
+// import { request } from 'graphql-request';
+
+export default {
+  name: 'IndvInvite',
+  props: {
+    loggedIn: Boolean,
+    invite: Object,
+  },
+  components: {
+
+  },
+};
+</script>
