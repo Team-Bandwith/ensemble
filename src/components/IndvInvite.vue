15a0a41f4877632c03f664bba8d3b0b6299bc9ff
Heather Haylett <heatherhaylett@gmail.com>
2020-05-16 14:32:20 -0500

    (feat) username has sent an invite with avatar

--- src/components/IndvInvite.vue
+++ src/components/IndvInvite.vue
@@ -3,8 +3,11 @@
     <div class="indv-invite">
         <b-row cols="12">
           <b-col>
-            <div class="invite-id">id:{{ invite.id }}</div>
-            <div class="user-from">title:{{ invite.id_user_from }}</div>
+            <div class="invite-avatar">
+              <img :src="invite.url_avatar">
+            </div>
+            <div class="invite-id">{{ invite.username }}
+            invited you to join their <a :href="'jam' + invite.link">jam room</a></div>
           </b-col>
         </b-row>
       </div>
