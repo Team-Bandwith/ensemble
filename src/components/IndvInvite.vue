c734218a274f567af7b53f6b505ff2d89af92515
Heather Haylett <heatherhaylett@gmail.com>
2020-05-16 15:55:10 -0500

    (feat) avatar to left of invite

--- src/components/IndvInvite.vue
+++ src/components/IndvInvite.vue
@@ -2,13 +2,13 @@
   <b-container fluid>
     <div class="indv-invite">
         <b-row cols="12">
-          <b-col>
-            <div class="invite-avatar">
-              <img :src="invite.url_avatar">
-            </div>
-            <div class="invite-id">{{ invite.username }}
-            invited you to join their <a :href="'jam' + invite.link">jam room</a></div>
-          </b-col>
+        <b-list-group style="width: 1000px;">
+        <b-list-group-item class="d-flex align-items-center">
+        <b-avatar variant="info" :src="invite.url_avatar" class="mr-3"></b-avatar>
+        <span class="mr-auto">{{ invite.username }}
+            invited you to join their <a :href="'jam' + invite.link">jam room</a></span>
+        </b-list-group-item>
+        </b-list-group>
         </b-row>
       </div>
   </b-container>
