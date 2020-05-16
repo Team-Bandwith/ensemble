cc211947db7ca14be18a62288de43ac4fc8580b3
Kenny-QD <KennyQDang@gmail.com>
2020-05-16 15:40:11 -0500

    created user song list component

---
+++ src/components/UserSongsList.vue
@@ -0,0 +1,75 @@
+/* eslint-disable max-len */
+<template>
+  <b-container fluid style="background-color:#98AC9E; width: 50%; margin-right: 50%; margin: 1px">
+    <div v-for="song in songs" :key="song.id">
+       <Song
+        :song="song"
+        :liked="liked"
+        :user="user"
+        v-on:new-like="newLike"
+      />
+      <hr>
+    </div>
+  </b-container>
+</template>
+<script>
+import { request } from 'graphql-request';
+import Song from './Song.vue';
+
+export default {
+  name: 'UserSongsList',
+  props: {
+    loggedIn: Boolean,
+    liked: Array,
+    user: Object,
+  },
+  components: {
+    Song,
+  },
+  data() {
+    return {
+      songs: [],
+    };
+  },
+  methods: {
+    getUserSongs() {
+      if (!this.loggedIn) {
+        this.songs = [];
+        return;
+      }
+      const query = `query {
+      getUserSongs(id: ${this.user.id}) {
+        id, 
+        id_author, 
+        name, 
+        url, 
+        count_likes,
+        created_at
+      }
+    }`;
+      request(`${process.env.NODE_ENV === 'development' ? 'http://localhost:8081' : ''}/api`, query)
+        .then((res) => {
+          this.songs = res.getUserSongs;
+        })
+        .catch((err) => console.log(err));
+    },
+    newLike() {
+      this.$emit('new-like');
+    },
+  },
+  created() {
+    this.getUserSongs();
+  },
+  watch: {
+    loggedIn() {
+      this.getUserSongs();
+    },
+  },
+};
+</script>
+
+<style scoped>
+b-container {
+  height: 50px;
+}
+</style>
