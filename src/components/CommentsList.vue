bab8f1c03e6f3d2afac9e363c28f1ee4bc519637
hartsean <hartsean@gmail.com>
2020-05-16 14:16:22 -0500

    (feat) implement comments, create commentsList component

---
+++ src/components/CommentsList.vue
@@ -0,0 +1,31 @@
+<template>
+<b-container>
+   <div v-for="comment in comments" :key="comment.id">
+       <Comment :comment="comment"></Comment>
+  </div>
+  <CommentInput></CommentInput>
+</b-container>
+</template>
+<script>
+import Comment from './Comment.vue';
+import CommentInput from './CommentInput.vue';
+
+export default {
+  props: {
+    id_user: Number,
+    text: String,
+  },
+  components: {
+    CommentInput,
+    Comment,
+  },
+  data() {
+    return {
+      comments: [{ id: 1, text: 'this is a comment' }, { id: 2, text: 'this is a comment2' }],
+    };
+  },
+};
+</script>
+<style scoped>
+
+</style>
