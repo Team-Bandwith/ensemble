9d85f9f4c6e1948f7fd7e9cfba7bee8852cd6f6f
hartsean <hartsean@gmail.com>
2020-05-18 17:47:10 -0500

    (feat) created getSongComments method to load comments for each song

--- src/components/CommentsList.vue
+++ src/components/CommentsList.vue
@@ -3,10 +3,12 @@
    <div v-for="comment in comments" :key="comment.id">
        <Comment :comment="comment"></Comment>
   </div>
-  <CommentInput></CommentInput>
+  <CommentInput v-on:new-comment="getSongComments" :song="song" :user="user"></CommentInput>
 </b-container>
 </template>
+
 <script>
+import request from 'graphql-request';
 import Comment from './Comment.vue';
 import CommentInput from './CommentInput.vue';
 
@@ -14,6 +16,8 @@ export default {
   props: {
     id_user: Number,
     text: String,
+    user: Object,
+    song: Object,
   },
   components: {
     CommentInput,
@@ -23,6 +27,27 @@ export default {
     return {
       comments: [],
     };
+  },
+  created() {
+    this.getSongComments();
+  },
+  methods: {
+    getSongComments() {
+      const query = `query {
+      getSongComments(id_song: ${this.song.id}) {
+        id, 
+        id_user, 
+        id_song, 
+        text,
+        created_at
+      }
+    }`;
+      request(`${process.env.NODE_ENV === 'development' ? 'http://localhost:8081' : ''}/api`, query)
+        .then((res) => {
+          this.comments = res.getSongComments;
+        })
+        .catch((err) => console.log(err));
+    },
   },
 };
 </script>
