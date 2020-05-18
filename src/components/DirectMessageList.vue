62217ec6cd52367fd5770911e869066cd08b30f8
Heather Haylett <heatherhaylett@gmail.com>
2020-05-18 16:03:09 -0500

    (feat) import Message History modal

--- src/components/DirectMessageList.vue
+++ src/components/DirectMessageList.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <h3>Direct Message</h3>
+    <div class="message-history">
+    <MessageHistory/>
+    </div>
   </div>
 </template>
 
 <script>
+import MessageHistory from './MessageHistory.vue';
+
 export default {
   name: 'direct-message',
   components: {
-
+    MessageHistory,
   },
 };
 </script>
