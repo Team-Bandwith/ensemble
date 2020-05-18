3f8964819fc50f03b0a9290ea330ccce963e2a3c
Heather Haylett <heatherhaylett@gmail.com>
2020-05-18 15:28:26 -0500

    (feat) file set up

---
+++ src/components/MessageHistory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-modal>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userFrom: '',
+      userTo: '',
+      messages: [
+        {
+          body: '',
+          author: '',
+        },
+      ],
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
