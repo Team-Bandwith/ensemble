e93687697ae1f72fac71831d3c15e4ffffa13112
Heather Haylett <heatherhaylett@gmail.com>
2020-05-18 22:18:00 -0500

    (feat) add file for each message

---
+++ src/components/DirectMessage.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container >
+    <b-list-group flush>
+      <div class="direct-message">
+        <b-list-group-item variant="dark">
+          <b-avatar :src="invite.url_avatar" class="mr-3"></b-avatar>
+            <span class="mr-auto">
+              {{ message.username }}
+            </span>
+        </b-list-group-item>
+      </div>
+    </b-list-group>
+  </b-container>
+</template>
+
+<script>
+
+// import { request } from 'graphql-request';
+
+export default {
+  name: 'IndvInvite',
+  props: {
+    loggedIn: Boolean,
+    message: Object,
+  },
+  components: {
+
+  },
+};
+</script>
