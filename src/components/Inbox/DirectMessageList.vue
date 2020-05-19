0c8e4f47f137ac73c2c529f947a2d2bdb24ff8fa
Heather Haylett <heatherhaylett@gmail.com>
2020-05-19 17:15:19 -0500

    (feat) grab all user DMs

--- src/components/Inbox/DirectMessageList.vue
+++ src/components/Inbox/DirectMessageList.vue
@@ -1,19 +1,55 @@
 <template>
-  <div>
-    <h3>Direct Message</h3>
-    <div class="message-history">
-    <DirectMessage/>
+  <b-container fluid>
+    <h3>Direct Messages</h3>
+    <div>
+      <span v-for="dm in dms" :key="dm.id">
+        <DirectMessage :dm="dm"></DirectMessage>
+      </span>
     </div>
-  </div>
+  </b-container>
 </template>
 
 <script>
+import { request } from 'graphql-request';
+
 import DirectMessage from './DirectMessage.vue';
 
 export default {
-  name: 'direct-message',
+  name: 'direct-message-list',
+  props: {
+    id: Number,
+  },
   components: {
     DirectMessage,
+  },
+  data() {
+    return {
+      dms: [],
+    };
+  },
+  methods: {
+    getUserDMs(id) {
+      const query = `query {
+        getUserDMs(id: ${id}) {
+          id,
+          id_user_to,
+          id_user_from,
+          text,
+          created_at,
+          username,
+          url_avatar
+        }
+      }`;
+      request(`${process.env.NODE_ENV === 'development' ? 'http://localhost:8081' : ''}/api`, query)
+        .then((res) => {
+          console.log(res.getUserDMs);
+          this.dms = res.getUserDMs;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  created() {
+    this.getUserDMs(this.id);
   },
 };
 </script>
