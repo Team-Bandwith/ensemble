eba40a8b8eeea91a525ae42ab6819704ca202789
Heather Haylett <heatherhaylett@gmail.com>
2020-05-22 16:35:59 -0500

    (fix) clean up console logs

--- src/components/MessageHistoryList.vue
+++ src/components/MessageHistoryList.vue
@@ -2,11 +2,15 @@
   <b-container >
     <b-list-group flush>
       <div class="indv-message">
-        <div>
+          <b-avatar
+          :src="message.url_avatar"
+          class="mr-3"
+          :class="[ Number(this.myId) === Number(this.message.id_user_from)
+          ? 'float-right' : 'float-left' ]"
+          ></b-avatar>
             <span class="mr-auto">
-              @{{message.username}}: {{ message.text }}
+              {{ message.text }}
             </span>
-        </div>
       </div>
     </b-list-group>
   </b-container>
@@ -21,9 +25,21 @@ export default {
   props: {
     loggedIn: Boolean,
     message: Object,
+    myId: Number,
+    userTo: String,
   },
   components: {
 
   },
+  data() {
+    console.log('myId:', this.myId);
+    console.log('user_from:', this.message.id_user_from);
+    return {
+    };
+  },
 };
 </script>
+
+<style scoped>
+
+</style>
