590523b9fe4b3d4939a22c1c80f90f1111adc1e8
Heather Haylett <heatherhaylett@gmail.com>
2020-05-26 16:29:02 -0500

    (feat) modal to edit bio

---
+++ src/components/EditBioModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-button id="show-btn" @click="$bvModal.show('edit-bio')">Edit Bio</b-button>
+    <b-modal
+      id="edit-bio"
+      title="Edit Bio"
+      hide-footer
+    >
+    <form>
+      <b-form-group>
+        <div class="current-bio">
+        Current Bio: {{this.bio}}
+        </div>
+      <b-form-input
+          class="edit-bio"
+          v-model="text"
+          placeholder="change your bio">
+
+      </b-form-input>
+      <b-button class="mt-3" @click="editBio">Submit Bio</b-button>
+      </b-form-group>
+    </form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'editBio',
+  props: {
+    bio: String,
+  },
+  data() {
+    return {
+      text: '',
+    };
+  },
+  methods: {
+    editBio() {
+      this.bio = this.text;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.current-bio {
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+}
+</style>
