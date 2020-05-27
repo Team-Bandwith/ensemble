a5e263f0a28cb644ba0fc1960a51367886579587
Heather Haylett <heatherhaylett@gmail.com>
2020-05-26 19:10:00 -0500

    (feat) add bio field to member table

--- schema.sql
+++ schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE member (
   username VARCHAR (50) UNIQUE NOT NULL,
   url_avatar VARCHAR (255),
   email VARCHAR (50) NOT NULL,
+  bio VARCHAR (255),
   password VARCHAR (255) NOT NULL
 );
 
