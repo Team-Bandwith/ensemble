e1492e3d05fee360ba65110cbebc8cffd18fbffd
hartsean <hartsean@gmail.com>
2020-05-26 21:52:17 -0500

    (chore) align message and useravatar on comment post

--- src/components/Comments/Comment.vue
+++ src/components/Comments/Comment.vue
@@ -1,21 +1,28 @@
 <template>
   <b-container fluid>
     <b-row>
-      <b-col cols="1">
-        <img v-if="comment.url_avatar" :src="comment.url_avatar" />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col cols="1">
-        <router-link :to="`/profile/${comment.id_user}`"> {{ comment.username }}</router-link>:
+      <b-col>
+        <div class="av-img">
+          <b-avatar v-if="comment.url_avatar" :src="comment.url_avatar" />
+        </div>
+        <div class="comment-text">
+          {{comment.text}}
+        </div>
       </b-col>
     </b-row>
     <b-row>
       <b-col>
-       {{comment.text}}
-      </b-col>
-      <b-col>
-        <em>{{handleMoment(comment.created_at).fromNow()}}</em>
+        <div class="spacer"></div>
+        <div class="creds">
+          <div class="user-link">
+            <router-link :to="`/profile/${comment.id_user}`">
+              {{ comment.username }}
+            </router-link>:
+          </div>
+          <div class="moment">
+            <em>{{handleMoment(comment.created_at).fromNow()}}</em>
+          </div>
+        </div>
       </b-col>
     </b-row>
   </b-container>
@@ -44,4 +51,31 @@ export default {
 </script>
 
 <style>
+.comment-text {
+  width: 82%;
+  float: left;
+  height: 100%;
+  padding: .5em;
+}
+.av-img  {
+  max-width: 18%;
+  float: left;
+}
+.spacer {
+  float: left;
+  width: 18%;
+}
+.creds{
+  width: 82%;
+  float: left;
+  height: 100%;
+  padding: .5px;
+}
+.moment{
+  float: left;
+}
+.user-link {
+  float: left;
+}
+
 </style>
