aa548769e8d213c8e06088a776e7d43596da3ffb
Heather Haylett <heatherhaylett@gmail.com>
2020-05-27 17:15:42 -0500

    (refactor) edit send button, center name under smaller image

--- src/components/SendInvite.vue
+++ src/components/SendInvite.vue
@@ -1,19 +1,26 @@
 <template>
-  <div v-show="value">
-    <div v-for="user in online" :key="user.id">
-      <img
-        v-if="!toInvite.map((usr) => usr.id).includes(user.id)"
-        :src="user.url_avatar"
-        @click="rsvp(user)"
-      >
-      <img
-        v-else
-        :src="user.url_avatar" class="selected"
-        @click="unrsvp(user)"
-      >
-      {{ user.username }}
-    </div>
-    <b-button @click="send">Send</b-button>
+  <div id="invite-container" v-show="value">
+    <b-col v-for="user in online" :key="user.id" id="invite-users">
+      <div id="center-wrapper">
+        <img
+          v-if="!toInvite.map((usr) => usr.id).includes(user.id)"
+          :src="user.url_avatar"
+          @click="rsvp(user)"
+        >
+        <img
+          v-else
+          :src="user.url_avatar"
+          class="selected"
+          @click="unrsvp(user)"
+        >
+        <div id="invite-name">
+        {{ user.username }}
+        </div>
+      </div>
+    </b-col>
+    <b-col cols="2">
+      <button id="send" @click="send" squared class="btn" type='submit'>Send</button>
+    </b-col>
   </div>
 </template>
 
@@ -36,6 +43,7 @@ export default {
   },
   methods: {
     rsvp(user) {
+      console.log('click');
       this.toInvite = [...this.toInvite, user];
     },
     unrsvp(user) {
@@ -45,12 +53,12 @@ export default {
       Promise.all(this.toInvite.map((usr) => {
         this.$socket.emit('notify', usr.id);
         const query = `
-  mutation {
-    sendInvite(id_user_to: ${usr.id}, id_user_from: ${this.you.id}, link: "${window.location.search}") {
-      id
-    }
-  }
-`;
+        mutation {
+          sendInvite(id_user_to: ${usr.id}, id_user_from: ${this.you.id}, link: "${window.location.search}") {
+            id
+          }
+        }
+      `;
         return request(`${process.env.NODE_ENV === 'development' ? 'http://localhost:8081' : ''}/api`, query);
       }))
         .then(() => {
@@ -63,16 +71,35 @@ export default {
 </script>
 
 <style scoped>
-div {
-  background-color: gray;
-}
-
-img {
-  height: 100px;
-  width: 100px;
-}
-.selected {
-  border-color: green;
-  border-style: solid;
+  #invite-container {
+    background-color: #3B3836;
+    border-radius: 5px;
+  }
+  #invite-name {
+    color: #99aca0;
+    text-align: center;
+  }
+  #invite-users {
+    margin-top: 10px;
+  }
+  #center-wrapper {
+    margin: auto;
+  }
+  #send {
+    background-color: #6d8657;
+    border-color: #6d8657;
+    color: white;
+    margin: 10px;
+    border-style: none;
+    border-radius: 0px;
+  }
+  .selected {
+    border-color: #6d8657;
+    border-style: solid;
+  }
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
 }
 </style>
