c5f957d9b06fe5f92185085ecce3fea71752860e
jwebber18 <jeremy.j.webber@gmail.com>
2020-05-27 19:08:37 -0500

    (refactor) Add drum sounds, dials

--- src/components/Drum.vue
+++ src/components/Drum.vue
@@ -1,6 +1,23 @@
 <template>
-  <b-container class="drum-toggle">
-    <b-row><div id="target"></div></b-row>
+  <b-container>
+    <b-row>
+        <div id="seq" />
+        <div id="rack1">
+        <div v-for="n in 3" :id="`select${3 - n + 1}`" :key="`select${n}`" />
+        </div>
+        <div id="rack2">
+        <div v-for="n in 3" :id="`pd${3 - n + 1}`" :key="`pd${n}`" />
+        </div>
+        <div id="rack3">
+        <div v-for="n in 3" :id="`oct${3 - n + 1}`" :key="`oct${n}`" />
+        </div>
+        <div id="rack4">
+        <div v-for="n in 3" :id="`att${3 - n + 1}`" :key="`att${n}`" />
+        </div>
+        <div id="rack5">
+        <div v-for="n in 3" :id="`dec${3 - n + 1}`" :key="`dec${n}`" />
+        </div>
+      </b-row>
     <b-button-group>
       <div class="play-btn">
         <b-button squared @click="playLoop">Play</b-button>
@@ -15,6 +32,7 @@
 <script>
 import Tone from 'tone';
 import Nexus from 'nexusui';
+import _ from 'lodash';
 
 export default {
   name: 'Drum',
@@ -25,10 +43,28 @@ export default {
     return {
       intervalState: false,
       sequencer: null,
-      instruments: ['C3', 'C4'],
+      instruments: ['C1', 'C2', 'G2'],
+      vols: [1, 1, 0.5],
       synths: [
-        new Tone.MembraneSynth().toMaster(),
-        new Tone.MembraneSynth().toMaster(),
+        new Tone.MembraneSynth({
+          pitchDecay: 0.15,
+          octaves: 13,
+          envelope: {
+            attack: 0.01,
+            decay: 0.55,
+          },
+        }).toMaster(),
+        new Tone.MembraneSynth({
+          oscillator: { type: 'triangle' },
+          pitchDecay: 0.05,
+          envelope: {
+            attack: 0.02,
+            decay: 0.35,
+          },
+        }).toMaster(),
+        new Tone.MembraneSynth({
+          oscillator: { type: 'sawtooth' },
+        }).toMaster(),
       ],
       col: null,
       transport: null,
@@ -37,19 +73,119 @@ export default {
   mounted() {
     Nexus.colors.accent = '#595959';
     Nexus.colors.fill = '#fff';
+    Tone.Transport.on('stop', () => console.log('STOP'));
+    Tone.Transport.on('pause', () => console.log('pause'));
+    document.documentElement.addEventListener('mousedown', () => {
+      if (Tone.context.state !== 'running') Tone.context.resume();
+    });
+
     this.createSequencer();
+
     Tone.Master.connect(this.dest);
+    /* eslint-disable no-new */
+    new Nexus.Rack('#rack1');
+    new Nexus.Rack('#rack2');
+    new Nexus.Rack('#rack3');
+    new Nexus.Rack('#rack4');
+    new Nexus.Rack('#rack5');
+
+    for (let i = 1; i < 4; i += 1) {
+      const select = new Nexus.Select(`#select${i}`, {
+        options: ['sine', 'triangle', 'sawtooth', 'square'],
+      });
+
+      select.selectedIndex = i - 1;
+
+      select.on('change', (s) => {
+        this.synths[i - 1].set('oscillator.type', s.value);
+        const vols = [...this.vols];
+        if (s.value === 'square' || s.value === 'sawtooth') {
+          vols[i - 1] = 0.5;
+        } else {
+          vols[i - 1] = 1;
+        }
+        this.vols = vols;
+      });
+    }
+
+    for (let i = 1; i < 4; i += 1) {
+      const pitchDecay = new Nexus.Dial(`#pd${i}`, {
+        size: [30, 30],
+        min: 0.05,
+        max: 0.15,
+        step: 0.01,
+        value: this.synths[i - 1].pitchDecay,
+      });
+
+      pitchDecay.on('change', (v) => {
+        if (this.synths[i - 1].pitchDecay !== v) {
+          this.synths[i - 1].set('pitchDecay', v);
+        }
+      });
+    }
+
+    for (let i = 1; i < 4; i += 1) {
+      const octaves = new Nexus.Dial(`#oct${i}`, {
+        size: [30, 30],
+        min: 5,
+        max: 15,
+        step: 1,
+        value: this.synths[i - 1].octaves,
+      });
+
+      octaves.on('change', (v) => {
+        if (this.synths[i - 1].octaves !== v) {
+          this.synths[i - 1].set('octaves', v);
+        }
+      });
+    }
+
+    for (let i = 1; i < 4; i += 1) {
+      const attack = new Nexus.Dial(`#att${i}`, {
+        size: [30, 30],
+        min: 0.001,
+        max: 0.2,
+        step: 0.01,
+        value: this.synths[i - 1].envelope.attack,
+      });
+
+      const changeAttack = _.debounce((val) => {
+        this.synths[i - 1].set('envelope.attack', val);
+      }, 500);
+
+      attack.on('change', (a) => {
+        changeAttack(a);
+      });
+    }
+
+    for (let i = 1; i < 4; i += 1) {
+      const decay = new Nexus.Dial(`#dec${i}`, {
+        size: [30, 30],
+        min: 0.1,
+        max: 1,
+        step: 0.05,
+        value: this.synths[i - 1].envelope.decay,
+      });
+
+      const changeDecay = _.debounce((val) => {
+        this.synths[i - 1].set('envelope.decay', val);
+      }, 500);
+
+      decay.on('change', (a) => {
+        changeDecay(a);
+      });
+    }
   },
   methods: {
     toggleIntervalState() {
       this.intervalState = !this.intervalState;
     },
     createSequencer() {
-      const sequencer = new Nexus.Sequencer('#target', {
-        size: [600, 200],
+      const sequencer = new Nexus.Sequencer('#seq', {
+        size: [400, 90],
         mode: 'toggle',
-        rows: 2,
-        columns: 12,
+        rows: 3,
+        columns: 16,
       });
       sequencer.on('step', (v) => {
         this.col = v;
@@ -67,10 +203,11 @@ export default {
                 drum: i,
                 room: this.$route.query.room,
               });
-              this.synths[i].triggerAttackRelease(this.instruments[i], '8n', time);
+              // const synth = new Tone.MembraneSynth().toMaster();
+              this.synths[i].triggerAttackRelease(this.instruments[i], '8n', time, this.vols[i]);
             }
           });
-        }, '0.5');
+        }, '0.25');
       }
       Tone.Transport.start();
     },
